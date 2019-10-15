2c87315b976bbf9177f4c5ab422d9ce3d228b7c8
Jarrell Houston <jhouston2882@yahoo.com>
2019-10-14 22:31:08 -0500

    added ability to make api call when clicking topic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 var topics = ["dogs", "cats", "fish", "bird"];
 
+topics.forEach(function (topic) {
+    $("#topics").append("<span class='badge badge-pill badge-info' data-topic='" + topic + "'>" + topic + "</span>");
+});
+
+
+
+
 var API_KEY = "AEcrIDQZBfcf8ogKoXMtc4w1sI0H8imE";
-var limit = "";
+var limit = "10";
 var query = "dogs";
 var rating = "";
 var URL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&limit=" + limit + "&rating=" + rating + "&api_key=" + API_KEY;
 
-$.ajax({
-    url: URL,
-    type: "GET"
-}).then(function (response) {
-    console.log(response);
+$("#topics").on("click", ".badge", function () {
+
+    query = $(this).attr("data-topic");
+    URL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&limit=" + limit + "&rating=" + rating + "&api_key=" + API_KEY;
+
+    $.ajax({
+        url: URL,
+        type: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var image = response.data[0].images.fixed_height.url;
+
+        $("#topic-gifs").append("<img src='" + image + "' alt='' class='img-thumbnail'>");
+    })
 })
+
